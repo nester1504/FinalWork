0f4cf550ead892fef97880e31a12961da49c26f5
Alexandr Nesterenko <nesteronok@gmail.com>
2023-03-23 00:35:41 +0600

    Метод считываения с клавиатуры

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿/// <summary>
+/// Медот считывает ввод с клавиатуры
+/// и делает проверку на ввод только целых положительных чисел
+/// </summary>
+/// <returns>Целое число</returns>
+int ReadNumber()
+{
+    bool check = false;
+    int number = 0;
+    while (check == false)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            if (number <= 0)
+            {
+                Console.WriteLine("Число должно быть больше нуля");
+            }
+            else
+            {
+                check = true;
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Не удалось распознать число, поробуйте ещё раз");
+        }
+    }
+    check = false;
+    return number;
+}
